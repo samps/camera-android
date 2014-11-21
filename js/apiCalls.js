4c334eb3e05a1d7ca26b4337bbcf3ef6cf8f2ef6
samps <sampsokholm@gmail.com>
2014-11-21 14:55:30 +1030

    Set up two buttons and some functionality to grab photos

--- js/apiCalls.js
+++ js/apiCalls.js
@@ -1,2 +1,29 @@
  
-//add code here to handle camera
+//add code here to handle camerafunction 
+
+TakePhotoUsingCamera() {
+	TakePhoto(Camera.PictureSourceType.CAMERA);
+}
+
+TakePhotoFromLibrary() {
+	TakePhoto(Camera.PictureSourceType.PHOTOLIBRARY);
+}
+
+function onSuccess(imageData) {
+	var image = document.getElementById('myImage');
+	image.src = 'data:image/jpeg;base64,' + imageData;
+}
+
+function onFail(message) {
+	alert('Failed because: ' + message);
+}
+
+function TakePhoto(sourceType) {
+	var camOptions = {
+		quality:50,
+		destinationType: Camera.DestinationType.DATA_URL,
+		sourceType: sourceType,
+		correctOrientation: true
+	};
+navigator.camera.getPicture(onSuccess, onFail, camOptions);
+}
